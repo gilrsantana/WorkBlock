1f718505892446b501a24a19f91946479f9373b5
Gilmar Santana <g.r.santana@hotmail.com>
2023-02-25 16:05:31 -0300

    Applying tests on PontoBlock

--- eth-contracts/test/workBlock/PontoBlock.ts
+++ eth-contracts/test/workBlock/PontoBlock.ts
@@ -52,16 +52,14 @@ describe('PontoBlock', () => {
     describe("Add a start of work", () => {
         it("should start of work", async () => {
             const { PontoBlockDeployed, 
-                    billy, 
-                    john, 
-                    alice } = await loadFixture(setupFixture);
-            const transaction1 = await PontoBlockDeployed.connect(billy).startWork();
-            const transaction2 = await PontoBlockDeployed.connect(john).startWork();
-            const transaction3 = await PontoBlockDeployed.connect(alice).startWork();
-            const regex = /^0x/;
-            expect(regex.test(transaction1.hash)).to.true;
-            expect(regex.test(transaction2.hash)).to.true;
-            expect(regex.test(transaction3.hash)).to.true;
+                    UtilDeployed,
+                    john } = await loadFixture(setupFixture);
+            PontoBlockDeployed.connect(john).startWork();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate = UtilDeployed.getDate((await block).timestamp);
+            const record = await (await PontoBlockDeployed.connect(john).getEmployeeRecords(john.address, myDate)).startWork;
+            expect(record.toNumber() > 0).to.true;
         });
         it("should not start work - Employee not registered", async () => {
             const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
@@ -83,19 +81,26 @@ describe('PontoBlock', () => {
      describe("Add an end of work", () => {
         it("should end of work", async () => {
             const { PontoBlockDeployed, 
-                    billy, 
-                    john, 
-                    alice } = await loadFixture(setupFixture);
+                    UtilDeployed,
+                    billy } = await loadFixture(setupFixture);
             await PontoBlockDeployed.connect(billy).startWork();
-            await PontoBlockDeployed.connect(john).startWork();
-            await PontoBlockDeployed.connect(alice).startWork();
-            const transaction1 = await PontoBlockDeployed.connect(billy).endWork();
-            const transaction2 = await PontoBlockDeployed.connect(john).endWork();
-            const transaction3 = await PontoBlockDeployed.connect(alice).endWork();
-            const regex = /^0x/;
-            expect(regex.test(transaction1.hash)).to.true;
-            expect(regex.test(transaction2.hash)).to.true;
-            expect(regex.test(transaction3.hash)).to.true;
+            await PontoBlockDeployed.connect(billy).endWork();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate = UtilDeployed.getDate((await block).timestamp);
+            const record = await (await PontoBlockDeployed.connect(billy).getEmployeeRecords(billy.address, myDate)).endWork;
+            expect(record.toNumber() > 0).to.true;
+        });
+        it("should end of work and break end time", async () => {
+            const { PontoBlockDeployed, UtilDeployed, billy } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(billy).startWork();
+            await PontoBlockDeployed.connect(billy).breakStartTime();
+            await PontoBlockDeployed.connect(billy).endWork();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate = UtilDeployed.getDate((await block).timestamp);
+            const record = await (await PontoBlockDeployed.connect(billy).getEmployeeRecords(billy.address, myDate)).breakEndTime;
+            await expect(record.toNumber() > 0).to.true;
         });
         it("should not end of work - Employee not registered", async () => {
             const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
@@ -121,65 +126,93 @@ describe('PontoBlock', () => {
         });
      });
      describe("Add an break start time", () => {
-        // it("should break start work", async () => {
-        //     const { PontoBlockDeployed, 
-        //             billy, 
-        //             john, 
-        //             alice } = await loadFixture(setupFixture);
-        //     const transaction1 = await PontoBlockDeployed.connect(billy).startWork();
-        //     const transaction2 = await PontoBlockDeployed.connect(john).startWork();
-        //     const transaction3 = await PontoBlockDeployed.connect(alice).startWork();
-        //     const regex = /^0x/;
-        //     expect(regex.test(transaction1.hash)).to.true;
-        //     expect(regex.test(transaction2.hash)).to.true;
-        //     expect(regex.test(transaction3.hash)).to.true;
-        // });
-        // it("should not start work - Employee not registered", async () => {
-        //     const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
-        //     await expect(PontoBlockDeployed.connect(jeff).startWork())
-        //             .to.rejectedWith("Employee not registered.");
-        // });
-        // it("should not start work - Employee is inactive", async () => {
-        //     const { PontoBlockDeployed, rachel } = await loadFixture(setupFixture);
-        //     await expect(PontoBlockDeployed.connect(rachel).startWork())
-        //         .to.rejectedWith("Employee is inactive.");
-        // });
-        // it("should not start work - Start of work already registered", async () => {
-        //     const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
-        //     await PontoBlockDeployed.connect(john).startWork();
-        //     await expect(PontoBlockDeployed.connect(john).startWork())
-        //         .to.rejectedWith("Start of work already registered.");
-        // });
+        it("should break start time", async () => {
+            const { PontoBlockDeployed, 
+                    UtilDeployed,
+                    billy } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(billy).startWork();
+            await PontoBlockDeployed.connect(billy).breakStartTime();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate = UtilDeployed.getDate((await block).timestamp);
+            const record = await (await PontoBlockDeployed.connect(billy).getEmployeeRecords(billy.address, myDate)).breakStartTime;
+            await expect(record.toNumber() > 0).to.true;
+        });
+        it("should not break start time - Employee not registered", async () => {
+            const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
+            await expect(PontoBlockDeployed.connect(jeff).breakStartTime())
+                    .to.rejectedWith("Employee not registered.");
+        });
+        it("should not break start time - Employee is inactive", async () => {
+            const { PontoBlockDeployed, rachel } = await loadFixture(setupFixture);
+            await expect(PontoBlockDeployed.connect(rachel).breakStartTime())
+                .to.rejectedWith("Employee is inactive.");
+        });
+        it("should not break start time - Start of break already registered", async () => {
+            const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(john).startWork();
+            await PontoBlockDeployed.connect(john).breakStartTime();
+            await expect(PontoBlockDeployed.connect(john).breakStartTime())
+                .to.rejectedWith("Start of break already registered.");
+        });
+        it("should not break start time - End of work already registered", async () => {
+            const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(john).startWork();
+            await PontoBlockDeployed.connect(john).endWork();
+            await expect(PontoBlockDeployed.connect(john).breakStartTime())
+                .to.rejectedWith("End of work already registered.");
+        });
      });
      describe("Add an break end time", () => {
-        // it("should start work", async () => {
-        //     const { PontoBlockDeployed, 
-        //             billy, 
-        //             john, 
-        //             alice } = await loadFixture(setupFixture);
-        //     const transaction1 = await PontoBlockDeployed.connect(billy).startWork();
-        //     const transaction2 = await PontoBlockDeployed.connect(john).startWork();
-        //     const transaction3 = await PontoBlockDeployed.connect(alice).startWork();
-        //     const regex = /^0x/;
-        //     expect(regex.test(transaction1.hash)).to.true;
-        //     expect(regex.test(transaction2.hash)).to.true;
-        //     expect(regex.test(transaction3.hash)).to.true;
-        // });
-        // it("should not start work - Employee not registered", async () => {
-        //     const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
-        //     await expect(PontoBlockDeployed.connect(jeff).startWork())
-        //             .to.rejectedWith("Employee not registered.");
-        // });
-        // it("should not start work - Employee is inactive", async () => {
-        //     const { PontoBlockDeployed, rachel } = await loadFixture(setupFixture);
-        //     await expect(PontoBlockDeployed.connect(rachel).startWork())
-        //         .to.rejectedWith("Employee is inactive.");
-        // });
-        // it("should not start work - Start of work already registered", async () => {
-        //     const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
-        //     await PontoBlockDeployed.connect(john).startWork();
-        //     await expect(PontoBlockDeployed.connect(john).startWork())
-        //         .to.rejectedWith("Start of work already registered.");
-        // });
+        it("should break end time", async () => {
+            const { PontoBlockDeployed, 
+                    UtilDeployed,
+                    billy, 
+                    john } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(john).startWork();
+            await PontoBlockDeployed.connect(john).breakStartTime();
+            await PontoBlockDeployed.connect(john).breakEndTime();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate = UtilDeployed.getDate((await block).timestamp);
+            const record = await (await PontoBlockDeployed.connect(john).getEmployeeRecords(john.address, myDate)).breakEndTime;
+            await expect(record.toNumber() > 0).to.true;
+        });
+        it("should not break end time - Employee not registered", async () => {
+            const { PontoBlockDeployed, jeff } = await loadFixture(setupFixture);
+            await expect(PontoBlockDeployed.connect(jeff).breakEndTime())
+                    .to.rejectedWith("Employee not registered.");
+        });
+        it("should not break end time - Employee is inactive", async () => {
+            const { PontoBlockDeployed, rachel } = await loadFixture(setupFixture);
+            await expect(PontoBlockDeployed.connect(rachel).breakEndTime())
+                .to.rejectedWith("Employee is inactive.");
+        });
+        it("should not break end time - End of break already registered", async () => {
+            const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(john).startWork();
+            await PontoBlockDeployed.connect(john).breakStartTime();
+            await PontoBlockDeployed.connect(john).breakEndTime();
+            await expect(PontoBlockDeployed.connect(john).breakEndTime())
+                .to.rejectedWith("End of break already registered.");
+        });
+        it("should not break end time - Start of break not registered", async () => {
+            const { PontoBlockDeployed, john } = await loadFixture(setupFixture);
+            await PontoBlockDeployed.connect(john).startWork();
+            await expect(PontoBlockDeployed.connect(john).breakEndTime())
+                .to.rejectedWith("Start of break not registered.");
+        });
      });
+    describe("get a contract date", () => {
+        it("should get a contract date", async () => {
+            const { PontoBlockDeployed, 
+                    UtilDeployed } = await loadFixture(setupFixture);
+            const contractDate = await PontoBlockDeployed.getCreationDateContract();
+            const blkNumber = ethers.provider.getBlockNumber();
+            const block = ethers.provider.getBlock(blkNumber);
+            const myDate =await UtilDeployed.getDate((await block).timestamp);
+            await expect(myDate.toNumber() === contractDate.toNumber()).to.true;
+        });
+    });
+
 });
