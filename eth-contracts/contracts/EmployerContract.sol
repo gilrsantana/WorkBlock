9740bb5fdb5c4660c0bf96073f941dd3f5748499
Gilmar Santana <g.r.santana@hotmail.com>
2023-03-26 06:08:30 -0300

    including modifier and event

--- eth-contracts/contracts/EmployerContract.sol
+++ eth-contracts/contracts/EmployerContract.sol
@@ -5,6 +5,10 @@ import "./AdministratorContract.sol";
 
 contract EmployerContract {
 
+    AdministratorContract private admin;
+    mapping (uint256 => Employer) private employers;
+    address[] private addsEmployers;
+
     struct Employer {
         uint256 idEmployer;
         address employerAddress;
@@ -13,17 +17,46 @@ contract EmployerContract {
         string legalAddress;
     }
 
-    AdministratorContract private admin;
-    mapping (uint256 => Employer) private employers;
-    address[] private addsEmployers;
+    event EmployerAdded(address indexed from_, 
+                        address indexed address_, 
+                        string name_, 
+                        uint256 taxId_, 
+                        string legalAddress_,
+                        uint256 timestamp_);
+
+    event EmployerUpdated(address indexed from_, 
+                          address indexed oldAddress_, 
+                          address indexed newAddress_, 
+                          string name_, 
+                          uint256 taxId_, 
+                          string legaAddress_,
+                          uint256 timestamp_);
 
     constructor(address _adm) {
         admin = AdministratorContract(_adm);
     }
 
-    function addEmployer(address _address, uint256 _taxId, string memory _name, string memory _legalAddress) public{
+    modifier onlyAdmin {
         require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+        _;
+    }
+
+    modifier employerNotAddedYet(address _address) {
         require(!checkIfEmployerExists(_address), "Employer already exists.");
+        _;
+    }
+
+    modifier employerAddedYet(address _address) {
+        require(checkIfEmployerExists(_address), "Employer not exists.");
+        _;
+    }
+
+    function addEmployer
+             (address _address, uint256 _taxId, string memory _name, string memory _legalAddress) 
+             public 
+             onlyAdmin()
+             employerNotAddedYet(_address) {
+
         require(_taxId != 0, "TaxId not given.");
         require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked("")), "Name not given.");
         require(keccak256(abi.encodePacked(_legalAddress)) != keccak256(abi.encodePacked("")), "Legal address not given.");
@@ -31,15 +64,20 @@ contract EmployerContract {
 
         employers[addsEmployers.length] = Employer(addsEmployers.length, _address, _taxId, _name, _legalAddress);
         addsEmployers.push(_address);
+
+        emit EmployerAdded(msg.sender, _address, _name, _taxId, _legalAddress, block.timestamp);
     }
 
-    function updateEmployer (address _addressKey, address _address, uint256 _taxId, string memory _name, string memory _legalAddress) public {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function updateEmployer 
+             (address _addressKey, address _address, uint256 _taxId, string memory _name, string memory _legalAddress) 
+             public 
+             onlyAdmin()
+             employerAddedYet(_addressKey) {
+
         require(_address != address(0), "Address not given.");
         require(_taxId != 0, "TaxId not given.");
         require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked("")), "Name not given.");
         require(keccak256(abi.encodePacked(_legalAddress)) != keccak256(abi.encodePacked("")), "Legal address not given.");
-        require(checkIfEmployerExists(_addressKey), "Employer not exists.");
 
         bool difAdd;
         address add;
@@ -67,15 +105,25 @@ contract EmployerContract {
                 }
             }
         }
+
+        emit EmployerUpdated(msg.sender, _addressKey, _address, _name, _taxId, _legalAddress, block.timestamp);
     }
 
-    function getEmployerById(uint256 _id) public view returns(Employer memory) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function getEmployerById
+             (uint256 _id) 
+             public view 
+             onlyAdmin()
+             returns (Employer memory) {
+
         return employers[_id];
     }
 
-    function getEmployerByAddress(address _address) public view returns (Employer memory) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function getEmployerByAddress
+             (address _address) 
+             public view 
+             onlyAdmin()
+             returns (Employer memory) {
+        
         Employer memory e;
         for (uint256 i = 0; i < addsEmployers.length; i++) {
             if (addsEmployers[i] == _address) {
@@ -86,8 +134,11 @@ contract EmployerContract {
         return e;
     }
     
-    function getAllEmployers() public view returns (Employer[] memory) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function getAllEmployers() 
+             public view 
+             onlyAdmin()
+             returns (Employer[] memory) {
+
         Employer[] memory result = new Employer[](addsEmployers.length);
         for (uint i = 0; i < addsEmployers.length; i++) {
             result[i] = employers[i];
@@ -95,8 +146,12 @@ contract EmployerContract {
         return result;
     }
     
-    function checkIfEmployerExists(address _address) public view returns (bool){
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function checkIfEmployerExists
+             (address _address) 
+             public view 
+             onlyAdmin()
+             returns (bool){
+
         for (uint i = 0; i < addsEmployers.length; i++)
             if(addsEmployers[i] == _address)
                 return true;
