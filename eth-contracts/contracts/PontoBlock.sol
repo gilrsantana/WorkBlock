88c9dca06be31f633d830697798b9900a5462ce7
Gilmar Santana <g.r.santana@hotmail.com>
2023-03-26 07:34:53 -0300

    including modifier and evet

--- eth-contracts/contracts/PontoBlock.sol
+++ eth-contracts/contracts/PontoBlock.sol
@@ -6,6 +6,16 @@ import "./UtilContract.sol";
 import "./AdministratorContract.sol";
 
 contract PontoBlock {
+    
+    EmployeeContract private employee;
+    UtilContract private util;
+    AdministratorContract private admin;
+
+    int private timeZone;
+    int private oneHour = 3600;
+    address private owner;
+    uint private creationDate;
+    mapping(address => mapping(uint256 => EmployeeRecord)) private employeeRecords;
 
     struct EmployeeRecord {
         uint256 startWork;
@@ -14,15 +24,10 @@ contract PontoBlock {
         uint256 breakEndTime;
     }
 
-    int private timeZone;
-    int private oneHour = 3600;
-    address private owner;
-    uint private creationDate;
-    mapping(address => mapping(uint256 => EmployeeRecord)) private employeeRecords;
- 
-    EmployeeContract private employee;
-    UtilContract private util;
-    AdministratorContract private admin;
+    event StartWorkRegistered(address indexed address_, uint256 indexed startWork_, uint256 timestamp_);
+    event EndWorkRegistered(address indexed address_, uint256 indexed endWork_, uint256 timestamp_);
+    event BreakStartWorkRegistered(address indexed address_, uint256 indexed breakStartWork_, uint256 timestamp_);
+    event BreakEndWorkRegistered(address indexed address_, uint256 indexed breakEndtWork_, uint256 timestamp_);
 
     constructor(address _emp, address _util, address _adm, int _timeZone) {
         employee = EmployeeContract(_emp);
@@ -38,17 +43,37 @@ contract PontoBlock {
         _;
     }
 
-    function startWork() public {
-        require(employee.checkIfEmployeeExists(msg.sender), "Employee not registered.");
-        require(employee.getEmployeeByAddress(msg.sender).stateOf == EmployeeContract.State.Active, "Employee is inactive.");
-        require(employeeRecords[msg.sender][util.getDate(getMoment())].startWork == 0, "Start of work already registered.");
-        
-        employeeRecords[msg.sender][util.getDate(getMoment())].startWork = getMoment();
+    modifier onlyAdmin() {
+        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+        _;
     }
 
-    function endWork() public {
-        require(employee.checkIfEmployeeExists(msg.sender), "Employee not registered.");
+    modifier employeeAddedYet(address _address) {
+        require(employee.checkIfEmployeeExists(_address), "Employee not registered.");
+        _;
+    }
+
+    modifier activeEmployee() {
         require(employee.getEmployeeByAddress(msg.sender).stateOf == EmployeeContract.State.Active, "Employee is inactive.");
+        _;
+    }
+
+    function startWork() 
+        public 
+        employeeAddedYet(msg.sender) 
+        activeEmployee() {
+        
+        require(employeeRecords[msg.sender][util.getDate(getMoment())].startWork == 0, "Start of work already registered.");
+        employeeRecords[msg.sender][util.getDate(getMoment())].startWork = getMoment();
+
+        emit StartWorkRegistered(msg.sender, employeeRecords[msg.sender][util.getDate(getMoment())].startWork, block.timestamp);
+    }
+
+    function endWork() 
+        public
+        employeeAddedYet(msg.sender) 
+        activeEmployee() {
+
         require(employeeRecords[msg.sender][util.getDate(getMoment())].endWork == 0, "End of work already registered.");
         require(employeeRecords[msg.sender][util.getDate(getMoment())].startWork != 0, "Start of work not registered.");
 
@@ -69,39 +94,58 @@ contract PontoBlock {
         }
 
         employeeRecords[msg.sender][util.getDate(getMoment())].endWork = getMoment();
+
+        emit EndWorkRegistered(msg.sender, employeeRecords[msg.sender][util.getDate(getMoment())].endWork, block.timestamp);
     }
 
-    function breakStartTime() public {
-        require(employee.checkIfEmployeeExists(msg.sender), "Employee not registered.");
-        require(employee.getEmployeeByAddress(msg.sender).stateOf == EmployeeContract.State.Active, "Employee is inactive.");
+    function breakStartTime() 
+        public
+        employeeAddedYet(msg.sender) 
+        activeEmployee() {
+
         require(employeeRecords[msg.sender][util.getDate(getMoment())].breakStartTime == 0, "Start of break already registered.");
         require(employeeRecords[msg.sender][util.getDate(getMoment())].startWork != 0, "Start of work not registered.");
         require(employeeRecords[msg.sender][util.getDate(getMoment())].endWork == 0, "End of work already registered.");
 
         employeeRecords[msg.sender][util.getDate(getMoment())].breakStartTime = getMoment();
+
+        emit BreakStartWorkRegistered(msg.sender, employeeRecords[msg.sender][util.getDate(getMoment())].breakStartTime, block.timestamp);
     }
 
-    function breakEndTime() public {
-        require(employee.checkIfEmployeeExists(msg.sender), "Employee not registered.");
-        require(employee.getEmployeeByAddress(msg.sender).stateOf == EmployeeContract.State.Active, "Employee is inactive.");
+    function breakEndTime() 
+        public
+        employeeAddedYet(msg.sender) 
+        activeEmployee() {
+
         require(employeeRecords[msg.sender][util.getDate(getMoment())].breakEndTime == 0, "End of break already registered.");
         require(employeeRecords[msg.sender][util.getDate(getMoment())].breakStartTime != 0, "Start of break not registered.");
 
         employeeRecords[msg.sender][util.getDate(getMoment())].breakEndTime = getMoment();
+
+        emit BreakEndWorkRegistered(msg.sender, employeeRecords[msg.sender][util.getDate(getMoment())].breakEndTime, block.timestamp);
     }
 
-    function getCreationDateContract() public view returns(uint) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function getCreationDateContract() 
+        public view 
+        onlyAdmin()
+        returns(uint) {
+
         return creationDate;
     }
 
-    function getEmployeeRecords(address _address, uint256 _date) public view returns (EmployeeRecord memory) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
-        require(employee.checkIfEmployeeExists(_address), "Employee not registered.");
+    function getEmployeeRecords (address _address, uint256 _date) 
+        public view 
+        onlyAdmin()
+        employeeAddedYet(_address)
+        returns (EmployeeRecord memory) {
+
         return employeeRecords[_address][_date];
     }
 
-    function getMoment() public view returns(uint256) {
+    function getMoment() 
+        public view 
+        returns(uint256) {
+
         int adjust = timeZone * oneHour;
         uint256 moment;
         if (adjust < 0) {
@@ -113,16 +157,24 @@ contract PontoBlock {
         return moment;
     }
 
-    function changeOwner(address _newOwner) public onlyOwner{
+    function changeOwner(address _newOwner) 
+        public 
+        onlyOwner() {
         owner = _newOwner;
     }
 
-    function getOwner() public view returns (address) {
-        require(admin.checkIfAdministratorExists(msg.sender), "Sender is not administrator.");
+    function getOwner() 
+        public view 
+        onlyAdmin()
+        returns (address) {
+
         return owner;
     }
 
-    function getTimeZone() public view returns (int) {
+    function getTimeZone() 
+        public view 
+        returns (int) {
+
         return timeZone;
     }
 
