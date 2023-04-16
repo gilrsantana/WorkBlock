fc0552a20aa11c62ebb118eaa097ea51f1b08aa8
Gilmar Santana <g.r.santana@hotmail.com>
2023-04-16 05:40:30 -0300

    Refactoring deployPontoBlockReports

--- eth-contracts/scripts/deploy/deployPontoBlockReports.ts
+++ eth-contracts/scripts/deploy/deployPontoBlockReports.ts
@@ -2,22 +2,35 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { ethers } from "hardhat";
 import { handler } from "../util/handlerEnv";
+import { AppDataSource } from "../../src/database/data-source";
+import { IContractService } from "../../src/interface/IContractService";
+import { contractModel } from "../../src/models/contractModel";
+import { contractService } from "../../src/service/contractService";
+import { IContractModel } from "../../src/interface/IContractModel";
+import { utilTools } from "../../src/util/utilTools";
 
 async function toDeployPontoBlockReportsContract() {
-    const employee = process.env.EMPLOYEE_ADDRESS ?? '';
-    const ponto = process.env.PONTOBLOCK_ADDRESS ?? '';
-    const util = process.env.UTIL_ADDRESS ?? '';
-    const admin = process.env.ADMINISTRATOR_ADDRESS ?? '';
+    const service: IContractService = new contractService(new contractModel());
+    const employee: any = await service.getContractByName("EmployeeContract");
+    const ponto: any = await service.getContractByName("PontoBlock");
+    const util: any = await service.getContractByName("UtilContract");
+    const administrator: any = await service.getContractByName("AdministratorContract");
+
+    const employeeAddress = employee.addressContract;
+    const pontoAddresss = ponto.addressContract;
+    const utilAddress = util.addressContract;
+    const administratorAddress = administrator.addressContract;
     const PontoBlockReports = await ethers.getContractFactory("PontoBlockReports");
-    const pontoBlockReports = await PontoBlockReports.deploy(employee, ponto, util, admin);
-    await pontoBlockReports.deployed()
-    const key = "PONTOBLOCKREPORTS_ADDRESS";
-    handler(key, pontoBlockReports.address)
-    console.log('\nPontoBlockReports successfull deployed at: ' + pontoBlockReports.address);
+    const pontoBlockReports = await PontoBlockReports.deploy(employeeAddress, pontoAddresss, utilAddress, administratorAddress);
+    await pontoBlockReports.deployed();
+
+    const contract: IContractModel = utilTools.buildContract('artifacts/contracts/PontoBlockReports.sol/PontoBlockReports.json', pontoBlockReports.address.toString())
+    await utilTools.handlerService(service, contract);
 }
 
 toDeployPontoBlockReportsContract().catch((error) => {
     console.error('Error at toDeployPontoBlockReportsContract:' + error);
     process.exitCode = 1;
+    AppDataSource.destroy();
 });
 
