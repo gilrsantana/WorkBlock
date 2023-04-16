40a17d1adc3ebdbdf9845e1848f50d1d7df6fb8d
Gilmar Santana <g.r.santana@hotmail.com>
2023-04-16 05:18:55 -0300

    Refactoring deployEmployee

--- eth-contracts/scripts/deploy/deployEmployee.ts
+++ eth-contracts/scripts/deploy/deployEmployee.ts
@@ -2,19 +2,27 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { ethers } from "hardhat";
 import { handler } from "../util/handlerEnv"
+import { IContractService } from "../../src/interface/IContractService";
+import { contractModel } from "../../src/models/contractModel";
+import { contractService } from "../../src/service/contractService";
+import { IContractModel } from "../../src/interface/IContractModel";
+import { utilTools } from "../../src/util/utilTools";
 
 async function toDeployEmployeeContract() {
-    const admAddress = process.env.ADMINISTRATOR_ADDRESS ?? '';
-    const emprAddress = process.env.EMPLOYER_ADDRESS ?? '';
-    const utilAddress = process.env.UTIL_ADDRESS ?? '';
+    const service: IContractService = new contractService(new contractModel());
+    const administrator: any = await service.getContractByName("AdministratorContract");
+    const employer: any = await service.getContractByName("EmployerContract");
+    const util: any = await service.getContractByName("UtilContract");
+
+    const admAddress = administrator.addressContract;
+    const emprAddress = employer.addressContract;
+    const utilAddress = util.addressContract;
     const Employee = await ethers.getContractFactory("EmployeeContract");
     const employee = await Employee.deploy(admAddress,emprAddress, utilAddress);
-    await employee.deployed()
-    const key = "EMPLOYEE_ADDRESS";
+    await employee.deployed();
 
-    handler(key, employee.address)
-    console.log('\nEmployee successfull deployed at: ' + employee.address);
-
+    const contract: IContractModel = utilTools.buildContract('artifacts/contracts/EmployeeContract.sol/EmployeeContract.json', employee.address.toString())
+    await utilTools.handlerService(service, contract);
 }
 
 toDeployEmployeeContract().catch((error) => {
