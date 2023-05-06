c6bd8a10f297e4ae52379061cee18dde5caf72bf
gilrsantana <g.r.santana@hotmail.com>
2023-05-06 07:57:42 -0300

    Extension Methods for Api Configuration

---
+++ WorkBlockApi/WorkBlockApi/Extensions/DependenciesExtension.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkBlockApi.Data;
+using WorkBlockApi.Interfaces;
+using WorkBlockApi.Repositories;
+
+namespace WorkBlockApi.Extensions;
+
+public static class DependenciesExtension
+{
+    public static void AddApiConfiguration(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        var apiConfiguration = new ApiConfiguration
+        {
+            PrivateKey = builder.Configuration.GetValue<string>("PrivateKey") ?? "",
+            Provider = builder.Configuration.GetValue<string>("Provider") ?? ""
+        };
+        services.AddSingleton(apiConfiguration);
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void AddContext(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<WorkBlockContext>(options =>
+        {
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        });
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IContractModelRepository, ContractModelRepository>();
+    }
+
+    public static void LoadConfiguration(this IServiceCollection services, WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
--- WorkBlockApi/WorkBlockApi/Program.cs
+++ WorkBlockApi/WorkBlockApi/Program.cs
@@ -1,35 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using WorkBlockApi;
-using WorkBlockApi.Data;
-using WorkBlockApi.Interfaces;
-using WorkBlockApi.Repositories;
+using WorkBlockApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var apiConfiguration = new ApiConfiguration
-    {
-        PrivateKey = builder.Configuration.GetValue<string>("PrivateKey") ?? "",
-        Provider = builder.Configuration.GetValue<string>("Provider") ?? ""
-    };
+builder.Services.AddApiConfiguration(builder);
+builder.Services.AddContext(builder);
+builder.Services.AddRepositories();
 
-builder.Services.AddTransient<IContractModelRepository, ContractModelRepository>();
-builder.Services.AddSingleton(apiConfiguration);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<WorkBlockContext>(options =>
-{
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
-
-var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-
-app.Run();
+builder.Services.LoadConfiguration(builder.Build());
