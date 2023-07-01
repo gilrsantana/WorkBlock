e955899d0c5a0dfa63a0c0ec4eb5649ba53a37d8
Gilmar Santana <g.r.santana@hotmail.com>
2023-06-30 23:22:02 -0300

    Refactoring code

--- WorkBlockApp/Versao2/WorkBlockApp/Models/ValueObjects/Documents/DocumentModel.cs
+++ WorkBlockApp/Versao2/WorkBlockApp/Models/ValueObjects/Documents/DocumentModel.cs
@@ -5,7 +5,7 @@ namespace WorkBlockApp.Models.ValueObjects.Documents;
 
 public abstract class DocumentModel : IDocument
 {
-    private static readonly Regex SWhitespace = new Regex(@"\s+");
+    private static readonly Regex SWhitespace = new Regex(@"\s+", RegexOptions.NonBacktracking);
     protected static string ChangeWhiteSpace(string input, string replacement)
     {
         return SWhitespace.Replace(input, replacement);
